61489b8c64da4a6ee75d5f39ec6711508833da81
adriesco <144839808+adriesco@users.noreply.github.com>
2024-11-11 15:55:56 +0100

    cambios

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  safelist: [
+    "theme-light:text-black",
+    "theme-light:bg-white",
+    "theme-light:border-gray-300", // Agrega más estilos según sea necesario
+  ],
+};
